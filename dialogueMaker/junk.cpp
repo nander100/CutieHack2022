84815a5dad278987eaca4d6a620565b1eb3c0237
Oliver Kim <kmyn7up@gmail.com>
2022-11-05 10:29:53 -0700

    Adding Junk

---
+++ dialogueMaker/junk.cpp
@@ -0,0 +1,21 @@
+
+// Ignore this file. Chances are it's pretty bad. I'm just practicing opening files and closing. 
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+string InputDialogue () {
+    string dialogue;
+    cin >> dialogue; 
+    return dialgoue;
+}
+
+string CreateOptions () {
+    string option;
+    cin >> option;
+    return option; 
+}
+
+int main {
