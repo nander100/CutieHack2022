96c2efd4305859b8859c994d0aec081b0e1a6849
Adithya Iyer <aiyer026@ucr.edu>
2022-11-05 11:55:05 -0700

    Created TreeTest Branch

---
+++ DialogNode.cpp
@@ -0,0 +1,6 @@
+#include "DialogNode.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
---
+++ DialogNode.h
@@ -0,0 +1,20 @@
+#ifndef _DIALOGNODE_H_
+#define _DIALOGENODE_H_
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class DialogNode {
+
+  public:
+
+  private:
+    string dialog;
+    vector<string> choices;
+    
+
+
+};
+
+#endif
