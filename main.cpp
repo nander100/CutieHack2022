a7009229b3811d6abc2a333e97c5fd5ea50027c1
Adithya Iyer <aiyer026@ucr.edu>
2022-11-05 19:43:09 -0700

    Fixed recursion

--- main.cpp
+++ main.cpp
@@ -3,50 +3,155 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
 const string promptDialog();
-const int promptPaths();
+const int promptPathways();
+const string promptDecision();
+const int promptDepth();
+void printVector(const vector<string>& v);
+void recurse (int depth, int counter, int numResults, int change, int index, vector<string>& v);
+
 int main() {
     
     // string s = promptDialog();
     // int i = promptPaths();
     // DialogNode root();
+    int recurseCounter = 1;
+    int depth = promptDepth();
+    int change = pow(2, (depth-2));
+    int numResults = pow(2, depth)-1;
+    int currIndex = (numResults-1)/2;
+    cout << numResults << endl;
+    vector<string> vec(numResults);
+    string first = promptDialog();
+    vec.insert(vec.begin() + currIndex, first);
+    //printVector(vec);
+    recurse(depth, recurseCounter, numResults, change, currIndex, vec);
 
-    ofstream fileOut;
-    fileOut.open("story.txt");
-
-    if (!fileOut.is_open()) {
-        cout << "Error creating output file" << endl;\
-        return 1; 
+    ofstream outFile("test.txt");
+    if (!outFile.is_open()) {
+        cout << "Error creating test.txt" << endl;
+        return 1;
     }
 
-    cout << "Enter your dialog (DONE to quit): " << endl;
-    string input;
-    while (input != "DONE") {
-        getline(cin, input);
-        if (input != "DONE") {
-            fileOut << input << "\n";
-        }
+    for (unsigned i = 0; i < vec.size(); ++i) {
+        outFile << vec.at(i);
     }
 
-    int pathways = 0;
-    cout << "Enter number of pathways: ";
-    cin >> pathways;
-    cout << endl;
+    outFile.close();
+    
+    
+    // ofstream fileOut;
+    // fileOut.open("story.txt");
 
-    int counter = 1;
-    while (pathways > 0) {
+    // if (!fileOut.is_open()) {
+    //     cout << "Error creating output file" << endl;\
+    //     return 1; 
+    // }
 
-        cout <<  "Option " << counter << endl;
-    }
+    // cout << "Enter your dialog (DONE to quit): " << endl;
+    // string input;
+    // while (input != "DONE") {
+    //     getline(cin, input);
+    //     if (input != "DONE") {
+    //         fileOut << input << "\n";
+    //     }
+    // }
+
+    // int pathways = promptPathways();
 
 
-    fileOut.close();
+    // int counter = 1;
+    // while (pathways > 0) {
+
+    //     cout <<  "Entering Option " << counter << "..."<< endl;
+    //     promptDecision();
+    //     promptDialog();
+    //     cout << "Enter Dialog"
+    //     counter++;
+    //     pathways--;
+
+    // }
+
+
+    // fileOut.close();
 
 
     //x.debugData();
 
     return 0;
+}
+
+const string promptDialog() {
+    ostringstream userInput;
+    string input;
+    cout << "Enter your dialog (DONE to stop): " << endl;
+    while (input != "DONE") {
+        getline(cin, input);
+        if (input != "DONE") {
+            userInput << input;
+        }
+        userInput << "\n";
+    }
+
+    return userInput.str();
+}
+
+const int promptPathways() {
+    int paths = 0;
+    cout << "Enter number of pathways: ";
+    cin >> paths;
+    cout << endl;
+    return paths;
+}
+
+const string promptDecision() {
+    string s = "";
+    return s;
+}
+
+const int promptDepth() {
+    int numResults = 0;
+    cout << "How many layers deep?: ";
+    cin >> numResults;
+    cout << endl;
+    return numResults;
+}
+
+void printVector(const vector<string>& v) {
+    cout << "Printing Vector: " << endl;
+    for (unsigned i = 0; i < v.size(); ++i) {
+        cout << v.at(i) << endl;
+    }
+}
+
+
+
+void recurse(int depth, int counter, int numResults, int change, int index, vector<string>& v) {
+    cout << "For OPTION " << counter << ": " << endl;
+    string x = promptDialog();
+    int tempUpIndex = index+change;
+    v.insert(v.begin() + tempUpIndex, x);
+    // cout << "change: " << change << endl;
+    // cout << "tempUp: " << tempUpIndex << endl;
+
+    cout << "For OPTION " << counter+1 << ": " << endl;
+    string y = promptDialog();
+    int tempDownIndex = index-change;
+    v.insert(v.begin() + tempDownIndex, y);
+    counter++;
+    // cout << "change: " << change << endl;
+    // cout << "tempDown" << tempDownIndex << endl;
+
+    //printVector(v);
+    if (counter >= depth) {
+        cout << "Completed!" << endl;
+    } else {
+        recurse(depth, counter, numResults, change/2, tempDownIndex, v);     
+        recurse(depth, counter, numResults, change/2, tempUpIndex, v);
+    }
+
 }
